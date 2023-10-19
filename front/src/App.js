f20d0f6c58980a0ca9ba2208445a15512959405e
charlottecolome <chacha.colome@sfr.fr>
2023-10-19 11:51:20 +0200

    tailwind

--- front/src/App.js
+++ front/src/App.js
@@ -3,22 +3,64 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="vertical-align">
+        <h1>Janvier</h1>
+          <div class="horizontal-align">
+
+            <div id="gauche">
+              <table>
+                <caption>Dépenses</caption>
+                <tr> 
+                    <th>Montant</th> 
+                    <th>Libelle</th>
+                    <th>Catégorie</th> 
+                </tr>
+              </table>
+              <button> + </button>
+            </div>
+
+            <div id="droite">
+              <div class="piechart">
+              </div>
+              <table>
+                  <tr> 
+                      <th>Dépenses</th> 
+                      <th id="depenses">0</th> 
+                  </tr>
+                  <tr> 
+                      <th>Revenu</th> 
+                      <th id="revenu">0</th> 
+                  </tr>
+                  <tr> 
+                      <th>Résultat</th> 
+                      <th id="résultat">0</th> 
+                  </tr>
+              </table>
+              <table>
+                <caption>Mois des dernieres années</caption>
+                <tr> 
+                  <td colspan="2">2021</td> 
+                  <td colspan="2">2022</td>
+                  <td colspan="2">2023</td> 
+                </tr>
+                  <tr>
+                    <tr> 
+                      <th>Dépenses</th> 
+                      <th id="depenses">0</th> 
+                    </tr>
+                    <tr> 
+                      <th>Revenu</th> 
+                      <th id="revenu">0</th> 
+                    </tr>
+                    <tr> 
+                      <th>Résultat</th> 
+                      <th id="résultat">0</th> 
+                    </tr>
+                  </tr>
+              </table>
+            </div>
+          </div>
+      </div>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./front/public/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
