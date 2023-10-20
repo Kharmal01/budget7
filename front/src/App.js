c119a6b35cc663eaf73e1ac77386996a8f26d245
charlottecolome <chacha.colome@sfr.fr>
2023-10-20 09:08:14 +0200

    test improve interface

--- front/src/App.js
+++ front/src/App.js
@@ -4,63 +4,71 @@ import './App.css';
 function App() {
   return (
     <div class="vertical-align">
+      <div class="espace">
+        <button id="precedent"> ⬅ </button>
         <h1>Janvier</h1>
-          <div class="horizontal-align">
+        <button id="suivant"> ➡ </button>
+      </div>
+      <div class="horizontal-align">
 
-            <div id="gauche">
-              <table>
-                <caption>Dépenses</caption>
-                <tr> 
-                    <th>Montant</th> 
-                    <th>Libelle</th>
-                    <th>Catégorie</th> 
-                </tr>
-              </table>
-              <button> + </button>
-            </div>
-
-            <div id="droite">
-              <div class="piechart">
-              </div>
-              <table>
-                  <tr> 
-                      <th>Dépenses</th> 
-                      <th id="depenses">0</th> 
-                  </tr>
-                  <tr> 
-                      <th>Revenu</th> 
-                      <th id="revenu">0</th> 
-                  </tr>
-                  <tr> 
-                      <th>Résultat</th> 
-                      <th id="résultat">0</th> 
-                  </tr>
-              </table>
-              <table>
-                <caption>Mois des dernieres années</caption>
-                <tr> 
-                  <td colspan="2">2021</td> 
-                  <td colspan="2">2022</td>
-                  <td colspan="2">2023</td> 
-                </tr>
-                  <tr>
-                    <tr> 
-                      <th>Dépenses</th> 
-                      <th id="depenses">0</th> 
-                    </tr>
-                    <tr> 
-                      <th>Revenu</th> 
-                      <th id="revenu">0</th> 
-                    </tr>
-                    <tr> 
-                      <th>Résultat</th> 
-                      <th id="résultat">0</th> 
-                    </tr>
-                  </tr>
-              </table>
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">Dépenses</h2>
+            <table>
+              <tr> 
+                  <th>Montant</th> 
+                  <th>Libelle</th>
+                  <th>Catégorie</th> 
+              </tr>
+            </table>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary" id="ajout"> + </button>
             </div>
           </div>
+        </div>
+
+        <div class="card w-96 bg-base-100 shadow-xl">
+          <div class="piechart">
+          </div>
+          <table>
+              <tr> 
+                  <th>Dépenses</th> 
+                  <th id="depenses">0</th> 
+              </tr>
+              <tr> 
+                  <th>Revenu</th> 
+                  <th id="revenu">0</th> 
+              </tr>
+              <tr> 
+                  <th>Résultat</th> 
+                  <th id="résultat">0</th> 
+              </tr>
+          </table>
+          <table>
+            <caption>Mois des dernieres années</caption>
+            <tr> 
+              <td colspan="2">2021</td> 
+              <td colspan="2">2022</td>
+              <td colspan="2">2023</td> 
+            </tr>
+              <tr>
+                <tr> 
+                  <th>Dépenses</th> 
+                  <th id="depenses">0</th> 
+                </tr>
+                <tr> 
+                  <th>Revenu</th> 
+                  <th id="revenu">0</th> 
+                </tr>
+                <tr> 
+                  <th>Résultat</th> 
+                  <th id="résultat">0</th> 
+                </tr>
+              </tr>
+          </table>
+        </div>
       </div>
+    </div>
   );
 }
 
